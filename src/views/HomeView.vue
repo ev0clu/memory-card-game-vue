0d33cd1f06b236130957aa317f0c97de0cdd14e0
ev0 <ev0clu@gmail.com>
2025-04-24 16:31:21 +0200

    HomeView component is updated, handleFormSubmit is reworked

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -3,23 +3,25 @@ import { ref, watch } from 'vue'
 import { toast } from 'vue-sonner'
 import router from '@/router'
 import { useStore } from '@/composable/useStore'
+import ContainerWrapper from '@/components/ContainerWrapper.vue'
+import type { Pokemon } from '@/types'
 
 const options = [
   {
     level: 'easy',
-    boardSize: 16, // 4x4 board
+    boardSize: 4, // 2x2 board
   },
   {
     level: 'medium',
-    boardSize: 36, // 6x6 board
+    boardSize: 6, // 2x3 board
   },
   {
     level: 'hard',
-    boardSize: 64, // 8x8 board
+    boardSize: 8, // 2x4 board
   },
 ]
 
-const { addPokemon } = useStore()
+const { addPokemons } = useStore()
 const difficultyLevel = ref('')
 const error = ref(false)
 const isLoading = ref(false)
@@ -28,30 +30,52 @@ function randomIntFromInterval(min: number, max: number) {
   return Math.floor(Math.random() * (max - min + 1) + min)
 }
 
+function shuffle(array: Pokemon[]) {
+  // Fisher-Yates Sorting Algorithm
+  for (let i = array.length - 1; i > 0; i--) {
+    const j = Math.floor(Math.random() * (i + 1))
+    ;[array[i], array[j]] = [array[j], array[i]]
+  }
+  return array
+}
+
+function duplicate(array: Pokemon[]) {
+  const duplicatedArray = array.reduce((res: Pokemon[], current) => [...res, current, current], [])
+  return duplicatedArray
+}
+
 async function handleFormSubmit() {
   if (difficultyLevel.value) {
     const selectedOption = options.find((option) => option.level === difficultyLevel.value)
 
     if (selectedOption) {
       isLoading.value = true
-      const idsArray: number[] = []
 
-      while (idsArray.length < selectedOption.boardSize / 2) {
+      const responseArray: Pokemon[] = []
+
+      while (responseArray.length < selectedOption.boardSize / 2) {
         const newPokemonId = randomIntFromInterval(1, 800)
-        const isExist = idsArray.find((element) => element === newPokemonId)
-        if (!isExist) idsArray.push(newPokemonId)
-      }
+        const isExist = responseArray.find((element) => element.id === newPokemonId)
+        if (!isExist) {
+          const response = await fetch(`https://pokeapi.co/api/v2/pokemon/${newPokemonId}`)
 
-      for (let i = 0; i < idsArray.length; i++) {
-        const response = await fetch(`https://pokeapi.co/api/v2/pokemon/${idsArray[i]}`)
+          if (response) {
+            const result = await response.json()
 
-        if (response) {
-          const result = await response.json()
-          addPokemon(result.name, result.sprites.other.dream_world.front_default)
-        } else {
-          toast.error('Fetch error!')
+            responseArray.push({
+              id: newPokemonId,
+              name: result.name,
+              imageUrl: result.sprites.other.home.front_default,
+            })
+          } else {
+            toast.error('Fetch error!')
+          }
         }
       }
+      const duplicatedResponseArray = duplicate(responseArray)
+      const shuffledDuplicatedResponseArray = shuffle(duplicatedResponseArray)
+
+      addPokemons(shuffledDuplicatedResponseArray)
       router.push('/game')
     } else {
       toast.error('Something went wrong. Try again!')
@@ -69,7 +93,7 @@ watch(difficultyLevel, (newVal) => {
 </script>
 
 <template>
-  <div class="home">
+  <ContainerWrapper>
     <img alt="Pokemon logo" src="@/assets/pokemon-logo.png" width="600" height="auto" />
     <form v-if="!isLoading" @submit.prevent="handleFormSubmit">
       <fieldset>
@@ -88,17 +112,10 @@ watch(difficultyLevel, (newVal) => {
       <button>Start Game</button>
     </form>
     <span v-else aria-busy="true" class="loader">Game is loading...</span>
-  </div>
+  </ContainerWrapper>
 </template>
 
 <style scoped>
-.home {
-  display: flex;
-  flex-direction: column;
-  justify-content: center;
-  align-items: center;
-}
-
 img {
   margin-bottom: 2rem;
 }
