c94d82d4d28cd0d34645f2848e8a202a7d301c5a
ev0 <ev0clu@gmail.com>
2025-04-24 16:29:01 +0200

    ContainerWrapper component is created

---
+++ src/components/ContainerWrapper.vue
@@ -0,0 +1,16 @@
+<script setup lang="ts"></script>
+
+<template>
+  <div class="container">
+    <slot></slot>
+  </div>
+</template>
+
+<style scoped>
+.container {
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+}
+</style>
