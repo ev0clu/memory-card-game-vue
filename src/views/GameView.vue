09aec0ad3c0175bb18fc61ab4e26efd43a774542
ev0 <ev0clu@gmail.com>
2025-04-30 12:17:11 +0200

    GameView is created

---
+++ src/views/GameView.vue
@@ -0,0 +1,100 @@
+<script setup lang="ts">
+import { computed, onBeforeMount, onMounted, onUnmounted, ref, watch } from 'vue'
+import CardComponent from '@/components/CardComponent.vue'
+import ContainerWrapper from '@/components/ContainerWrapper.vue'
+import { useStore } from '@/composable/useStore'
+import router from '@/router'
+import type { Pokemon } from '@/types'
+
+let intervalId: number | undefined
+
+const { pokemons, setPokemonFound } = useStore()
+const counter = ref(0)
+const pokemonPairs = ref<Pokemon[]>([])
+
+function setCounter() {
+  counter.value = counter.value + 1
+}
+
+function select(cardItem: Pokemon) {
+  pokemonPairs.value.push({ ...cardItem })
+}
+
+const isPair = computed(() => {
+  if (pokemonPairs.value.length == 2) {
+    return pokemonPairs.value[0].id === pokemonPairs.value[1].id
+  } else {
+    return false
+  }
+})
+
+const isGameOver = computed(() => pokemons.value.every((item) => item.isFound))
+
+watch(isPair, (newVal) => {
+  if (newVal) {
+    setPokemonFound(pokemonPairs.value[0].id)
+  }
+})
+
+watch(
+  pokemonPairs,
+  (newVal) => {
+    if (newVal.length === 2) {
+      setTimeout(() => {
+        pokemonPairs.value = []
+      }, 1000)
+    }
+  },
+  { deep: true },
+)
+
+watch(isGameOver, (newVal) => {
+  if (newVal) {
+    clearInterval(intervalId)
+    console.log('GAME OVER')
+  }
+})
+
+onBeforeMount(() => {
+  if (pokemons.value.length === 0) {
+    router.push('/')
+  }
+})
+onMounted(() => (intervalId = setInterval(setCounter, 1000)))
+onUnmounted(() => clearInterval(intervalId))
+</script>
+
+<template>
+  <ContainerWrapper>
+    <p>
+      <span>Elapsed Time: </span>
+      <span>{{ counter }}</span>
+      <span> s</span>
+    </p>
+    <div class="card-wrapper">
+      <CardComponent
+        v-for="(pokemon, index) in pokemons"
+        :key="pokemon.name + index"
+        :pokemon="pokemon"
+        :pokemon-pairs="pokemonPairs"
+        @select="select"
+      />
+    </div>
+  </ContainerWrapper>
+</template>
+
+<style scoped>
+span {
+  font-size: x-large;
+}
+
+span:nth-child(2) {
+  font-weight: 800;
+}
+
+.card-wrapper {
+  display: grid;
+  grid-template-columns: repeat(2, 1fr);
+  gap: 0.5rem;
+}
+</style>
